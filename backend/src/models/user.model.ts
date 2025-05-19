3d97032835ba5b7bdb9007b1b3735b43fe643709
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-05-19 14:26:22 +0700

    feat: add prisma db connection feat: add user model with findByEmail, createUser, updateUser methods

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,11 @@
+const findByEmail = async (email: string) => {};
+
+const createUser = async (email: string, password: string) => {};
+
+const updateUser = async (
+  userId: string,
+  userName: string,
+  password: string
+) => {};
+
+export { findByEmail, createUser, updateUser };
