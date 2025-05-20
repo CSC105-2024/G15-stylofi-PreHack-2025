64beb132645228a2c4a91ca6da0fa9cba8be803c
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-20 12:19:26 +0700

    feat: update routes and add toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import AuthLayout from "./pages/AuthLayout/AuthLayoutPage";
+import SignUpPage from "./pages/SignUp/SignUpPage.jsx";
+import SignInPage from "./pages/SignIn/SignInPage";
+import { Toaster } from "react-hot-toast";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/signup" replace />,
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "signup", element: <SignUpPage /> },
+      { path: "signin", element: <SignInPage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Toaster position="top-right" />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
