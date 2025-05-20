615ace601daded7062e6db1f755cf7a30737312a
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-20 16:18:29 +0700

    feat: update search input component

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -3,14 +3,16 @@ import { Search } from "lucide-react";
 
 const SearchInput = () => {
   return (
-    <div className="relative">
-      <Search className="w-4 h-4 absolute left-2.5 top-2.5 text-text dark:text-text" />
-      <Input
-        type="search"
-        placeholder="Search"
-        className="w-full pl-8 bg-input-background ring-primary"
-      />
-    </div>
+    <>
+      <div className="flex items-center justify-center sticky">
+        <Search className="w-4 h-4 absolute left-2 top-3 text-text dark:text-text" />
+        <Input
+          type="search"
+          placeholder="Search"
+          className="w-256 h-10 pl-8 bg-input-background ring-primary border-0"
+        />
+      </div>
+    </>
   );
 };
 
