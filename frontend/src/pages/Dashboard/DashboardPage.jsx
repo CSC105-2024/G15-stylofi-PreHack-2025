5e276750d84ce7ee2d149e6484c62cfbdee778cd
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-20 14:31:18 +0700

    feat: add dashboard page

---
+++ frontend/src/pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import SearchInput from "@/components/SearchInput";
+
+const DashboardPage = () => {
+  return (
+    <div className="p-4 flex justify-center items-center">
+      <SearchInput />
+    </div>
+  );
+};
+
+export default DashboardPage;
