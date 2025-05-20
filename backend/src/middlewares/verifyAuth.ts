a492e23e347606e229994f50b19928acb8d84d21
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-21 04:54:44 +0700

    feat: add verify Access & Refresh funcs

--- backend/src/middlewares/verifyAuth.ts
+++ backend/src/middlewares/verifyAuth.ts
@@ -1,27 +1,30 @@
 import { type Context, type Next } from "hono";
 import { getSignedCookie } from "hono/cookie";
-import { verifyToken } from "../utils/token.ts";
+import { verifyAccessToken } from "../utils/token.ts";
 
 export const verifyAuth = async (c: Context, next: Next) => {
+  const secret = process.env.JWT_SECRET_KEY!;
+
   try {
-    const cookie = await getSignedCookie(
-      c,
-      process.env.SECRET_COOKIE!,
-      "token",
-    );
+    const cookie = await getSignedCookie(c, secret, "token");
 
     if (typeof cookie !== "string") {
       throw new Error("Invalid or missing token");
     }
 
-    const payload = verifyToken(cookie);
+    const payload = verifyAccessToken(cookie);
 
-    if (!payload || typeof payload !== "object" || !payload.userId) {
+    if (
+      !payload ||
+      typeof payload !== "object" ||
+      !payload.id ||
+      !payload.email
+    ) {
       throw new Error("Token verification failed");
     }
 
-    c.set("userId", payload.userId);
-    c.set("userEmail", payload.userEmail);
+    c.set("userId", payload.id);
+    c.set("userEmail", payload.email);
     await next();
   } catch (e) {
     return c.json(
