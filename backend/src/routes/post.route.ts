67677c4894ff42e86959367c1d0d6b9086aefdae
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-22 20:53:04 +0700

    feat: update post route - have 2 endpoints for img uploading

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -1,23 +1,23 @@
 import { Hono } from "hono";
-import {
-  createPost,
-  deletePost,
-  getAllPosts,
-  getAllPostsByUser,
-  getPost,
-  updatePost,
-} from "../controllers/post.controller.ts";
+import * as postController from "../controllers/post.controller.ts";
 import { verifyAuth } from "../middlewares/verifyAuth.ts";
+import { validateImage } from "../middlewares/validateImage.ts";
+import { checkValidation } from "../middlewares/checkValidation.ts";
 
 const postRouter = new Hono();
 
 postRouter.use(verifyAuth);
 
-postRouter.get("/user", getAllPostsByUser);
-postRouter.get("/", getAllPosts);
-postRouter.get("/:id", getPost);
-postRouter.post("/create", createPost);
-postRouter.put("/:id", updatePost);
-postRouter.delete("/:id", deletePost);
+postRouter.get("/user", postController.getAllPostsByUser);
+postRouter.get("/", postController.getAllPosts);
+postRouter.get("/:id", postController.getPost);
+postRouter.post(
+  "/validate-image",
+  validateImage,
+  postController.returnValidated,
+);
+postRouter.post("/create", checkValidation, postController.createPost);
+postRouter.put("/:id", postController.updatePost);
+postRouter.delete("/:id", postController.deletePost);
 
 export { postRouter };
