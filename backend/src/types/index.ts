e9958584729aa101484be94db1b571b3545c708b
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-22 20:50:10 +0700

    fix: update posts like optional / link mandatory

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -2,8 +2,8 @@ export interface CreatePostInput {
   title: string;
   description?: string;
   authorId: string;
-  likes: number;
-  link?: string;
+  likes?: number;
+  link: string;
   imageUrl: string;
   tagIds: number[];
 }
