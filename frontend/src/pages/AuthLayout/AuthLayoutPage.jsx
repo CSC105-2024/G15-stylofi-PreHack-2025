0c24f3917dfe92f66fceb151099f1a19f7650751
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-25 09:42:39 +0700

    fix: remove useState

--- frontend/src/pages/AuthLayout/AuthLayoutPage.jsx
+++ frontend/src/pages/AuthLayout/AuthLayoutPage.jsx
@@ -3,7 +3,7 @@ import WallpaperGrid from "@/components/WallpaperGrid";
 import Logo from "@/components/Logo";
 import { AnimatePresence, motion } from "framer-motion";
 
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 const AuthLayout = () => {
   const location = useLocation();
