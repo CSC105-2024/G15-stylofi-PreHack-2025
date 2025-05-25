b9631c0bf26704f0ae517297733f649302e0cfbf
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-05-25 23:06:04 +0700

    feat: implement OTP fields in User model and user authentication
    This commit introduces the following changes:
    
    - Adds `otpCode`, `otpExpiresAt`, and `otpVerified` fields to the `User` model
      in `prisma/migrations/20250525150004_init/migration.sql`. These fields are
      used for implementing OTP-based authentication.
    - Updates `src/controllers/user.controller.ts` to handle user sign-in.

---
+++ backend/prisma/migrations/20250525150004_init/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `otpCode` VARCHAR(191) NULL,
+    ADD COLUMN `otpExpiresAt` DATETIME(3) NULL,
+    ADD COLUMN `otpVerified` BOOLEAN NOT NULL DEFAULT false;
