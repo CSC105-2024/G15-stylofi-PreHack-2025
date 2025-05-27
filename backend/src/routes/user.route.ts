b42d2bba2293917fa80e9e25e45c6971ee546e5a
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-05-27 11:18:17 +0700

    feat(user): implement get logged in user endpoint
    This commit introduces a new endpoint `/me` to retrieve the
    currently logged-in user's information. It uses the `verifyAuth`
    middleware to ensure that only authenticated users can access
    this endpoint. The `getLoggedInUser` controller function fetches
    the user data based on the user ID stored in the context and
    returns it in the response.
    The console log was removed.

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import * as userController from "../controllers/user.controller.ts";
 import { authCheck } from "../utils/authCheck.ts";
+import { verifyAuth } from "../middlewares/verifyAuth.ts";
 
 const userRouter = new Hono();
 
@@ -11,6 +12,7 @@ userRouter.post("/resend-otp", userController.resendOtp);
 userRouter.post("/signin", userController.signInUser);
 userRouter.post("/refresh", userController.refreshToken);
 userRouter.post("/signout", userController.signOutUser);
+userRouter.get("/me", verifyAuth, userController.getLoggedInUser);
 userRouter.get("/:id", userController.getUserById);
 
 export { userRouter };
