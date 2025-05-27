0c705f6295fb7c9b1c2ede8e48be7db4a3514de0
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-28 00:03:52 +0700

    fix: remove getUserById func

--- frontend/src/services/user.jsx
+++ frontend/src/services/user.jsx
@@ -1,10 +1,5 @@
 import api from './api';
 
-export const getUserById = async (userId) => {
-  const res = await api.get(`/auth/${userId}`);
-  return res.data;
-};
-
 export const getUserName = async (userId) => {
   const res = await api.get(`/auth/username/${userId}`);
   return res.data;
